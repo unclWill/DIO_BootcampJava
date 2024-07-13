4c784da480ee5a733fe4844deef5fc13446e611c
William Silva <william.silva@viannasempre.com.br>
2024-07-13 16:42:29 -0300

    Exercício de concatenação de Strings.

---
+++ trilha-java-basico/java-anatomia-classes/java-anatomia-classes/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Introdução ao Java
+ * 
+ * Data: 13/07/2024
+ * @author William Silva
+ */
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String meuNome = "William";
+        String meuSobrenome = "Silva";
+
+        System.out.println(nomeCompleto(meuNome, meuSobrenome));
+    }
+
+    public static String nomeCompleto(String nome, String sobrenome) {
+        return nome.concat(" ").concat(sobrenome);
+    }
+}
