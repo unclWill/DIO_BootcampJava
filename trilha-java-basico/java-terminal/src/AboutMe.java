23dfa192afe4f983c9c531ca78010f2c4d93558f
William Silva <william.silva@viannasempre.com.br>
2024-07-14 16:29:50 -0300

    Alteração na classe AboutMe para ustilização da classe Scanner.

--- trilha-java-basico/java-terminal/src/AboutMe.java
+++ trilha-java-basico/java-terminal/src/AboutMe.java
@@ -1,14 +1,39 @@
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) throws Exception {
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        Scanner sc = new Scanner(System.in);
+        
+        String nome = "";
+        String sobrenome = "";
+        int idade = 0;
+        double altura = 0.0;
 
+        if (args.length != 0) {
+            nome = args[0];
+            sobrenome = args[1];
+            idade = Integer.valueOf(args[2]);
+            altura = Double.valueOf(args[3]);
+        } else {
+            System.out.print("Digite seu nome: ");
+            nome = sc.nextLine();
+            System.out.print("Digite seu sobrenome: ");
+            sobrenome = sc.nextLine();
+            System.out.print("Digite sua idade: ");
+            idade = sc.nextInt();
+            System.out.print("Digite sua altura: ");
+            altura = sc.nextDouble();
+            sc.close();
+        }
+
+        imprimir(nome, sobrenome, idade, altura);
+    }
+
+    private static void imprimir(String nome, String sobrenome, int idade, double altura) {
         System.out.printf("""
                 Olá, me chamo %s %s
                 Tenho %d anos de idade
-                E tenho %.2fm de altura
+                E tenho %.2f m de altura
                 """, nome, sobrenome, idade, altura);
     }
 }
