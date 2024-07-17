6350782f9a26053302014dcfb316fd39b9ef3212
William Silva <william.silva@viannasempre.com.br>
2024-07-17 14:47:52 -0300

    Adição de exercício de Collections com manipulação de uma List de Inteiros

---
+++ java-collections/collections-java-api/src/main/java/edu/dio/list/somadenumeros/Main.java
@@ -0,0 +1,26 @@
+package edu.dio.list.somadenumeros;
+
+public class Main {
+    public static void main(String[] args) {
+        SomaNumeros somaNumeros = new SomaNumeros();
+
+        // Adicionando números ná lista.
+        somaNumeros.adicionarNumero(100);
+        somaNumeros.adicionarNumero(90);
+        somaNumeros.adicionarNumero(7);
+        somaNumeros.adicionarNumero(50);
+
+        // Calculando a soma dos números da lista.
+        System.out.printf("Soma do números = %d%n", somaNumeros.calcularSoma());
+
+        // Encontrando o maior número da lista
+        System.out.printf("Maior número da lista = %d%n", somaNumeros.encontrarMaiorNumero());
+
+        // Encontrando o menor número da lista.
+        System.out.printf("Menor número da lista = %d%n", somaNumeros.encontrarMenorNumero());
+
+        // Exibindo a lista de números.
+        System.out.println("Lista de números");
+        somaNumeros.exibirNumeros();
+    }
+}
---
+++ java-collections/collections-java-api/src/main/java/edu/dio/list/somadenumeros/SomaNumeros.java
@@ -0,0 +1,55 @@
+package edu.dio.list.somadenumeros;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomaNumeros {
+    private List<Integer> listaNumeros;
+
+    public SomaNumeros() {
+        listaNumeros = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+        listaNumeros.add(numero);
+    }
+
+    public int calcularSoma() {
+        int somaNumeros = 0;
+        if (!listaNumeros.isEmpty()) {
+            for (Integer numero : listaNumeros) {
+                somaNumeros += numero;
+            }
+        }
+        return somaNumeros;
+    }
+
+    public int encontrarMaiorNumero() {
+        int maiorNumero = listaNumeros.get(0);
+
+        for (Integer numeroAtual : listaNumeros) {
+            if (numeroAtual > maiorNumero) {
+                    maiorNumero = numeroAtual;
+            }
+        }
+        return maiorNumero;
+    }
+
+    public int encontrarMenorNumero() {
+        int menorNumero = listaNumeros.get(0);
+        for (Integer numeroAtual : listaNumeros) {
+            if (numeroAtual < menorNumero) {
+                menorNumero = numeroAtual;
+            }
+        }
+        return menorNumero;
+    }
+
+    public void exibirNumeros() {
+        if (!listaNumeros.isEmpty()) {
+            for (Integer numero : listaNumeros) {
+                System.out.println(numero);
+            }
+        }
+    }
+}
