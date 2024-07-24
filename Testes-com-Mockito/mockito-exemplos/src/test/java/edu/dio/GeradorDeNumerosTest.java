b24a32e60297799a6eb86b788b40dfd7cc5e2b72
William Silva <william.silva@viannasempre.com.br>
2024-07-24 14:09:51 -0300

    Fazendo testes com métodos estáticos utilizando o mockito-inline

---
+++ Testes-com-Mockito/mockito-exemplos/src/test/java/edu/dio/GeradorDeNumerosTest.java
@@ -0,0 +1,15 @@
+package edu.dio;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class GeradorDeNumerosTest {
+    @Test
+    void testaGeracaoComTamanhoDefinido() {
+        MockedStatic<GeradorDeNumeros> mockedStatic = Mockito.mockStatic(GeradorDeNumeros.class);
+    }
+}
